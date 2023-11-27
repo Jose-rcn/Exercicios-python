ffab001d242ffff2852953d61675dc2e4ffcc6f1
José Ribeiro da costa Neto <84649190+Jose-rcn@users.noreply.github.com>
2023-11-27 16:11:29 -0300

    Update letter-counter.py
    Adicionar a função de contar quantas vezes a letra mais repetida apareceu e melhorar o nome das variaveis

--- letter-counter.py
+++ letter-counter.py
@@ -1,10 +1,16 @@
 #Desenvolver um programa empython capaz de contar o caractere que mais se repete dentro de uma string
 phrase = input('Write a phrase:')
-most_repeated = phrase[0]
-counter = 0
-while counter < len(phrase):
-    count_actual_char = (phrase.count(phrase[counter]))
-    if count_actual_char > most_repeated.count(most_repeated):
-        most_repeated = phrase[counter]
-    counter += 1
-print(f'The most repeated letter in this sentence is "{most_repeated}"')
+most_repeated_char = ''
+count_most_repeated_char = 0
+index = 0
+while index < len(phrase):
+    if phrase[index] == ' ': #eliminar espaços
+        index+=1
+        continue
+    actual_char = phrase[index]
+    count_actual_char = phrase.count(actual_char)
+    if count_actual_char > count_most_repeated_char:
+        most_repeated_char = phrase[index]
+        count_most_repeated_char = count_actual_char
+    index += 1
+print(f'The most repeated letter in this sentence is "{most_repeated_char}" which ocurred {count_most_repeated_char} times')
