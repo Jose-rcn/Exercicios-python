4c72b5aebd1871dec680406a033c047cc57186ef
José Ribeiro da costa Neto <josercn55@gmail.com>
2023-12-10 19:20:41 -0300

    Update bubble_sort.py

--- src/bubble_sort.py
+++ src/bubble_sort.py
@@ -1,13 +1,13 @@
 #estudo sobre algoritmos de ordenação de arrays
-lista = 10,8,12, 12, 1, 2, 44, 3, 2, 90, 12, 13, 2
-aux = 0
-requests = 0
-for i in range(0, len(lista,),1):
-    for t in range(i +1, len(lista), 1):
-        if lista[i] > lista[t]:
-            aux = lista[i]
-            lista[i] = lista[t]
-            lista[t] = aux
-        requests +=1
+lista = [10,8,12, 12, 1, 2, 44, 3, 2, 90, 12, 13, 2]
+
+def bubble_sort(lista:list) -> list:
+    lista_ordenada = lista.copy()
+    for i in range(0, len(lista_ordenada,)):
+        for t in range(i +1, len(lista_ordenada)):
+            if lista_ordenada[i] > lista_ordenada[t]:
+                lista_ordenada[i], lista_ordenada[t] = lista_ordenada[t], lista_ordenada[i]
+    return lista_ordenada
+
+print(bubble_sort(lista))
 print(lista)
-print(requests)
