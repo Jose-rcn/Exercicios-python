45e4dfc77922160e19a2ddcf58926abfe25b9021
José Ribeiro da costa Neto <84649190+Jose-rcn@users.noreply.github.com>
2024-03-31 01:07:43 -0300

    Update bubble_sort.py
    add comentarios

--- src/bubble_sort.py
+++ src/bubble_sort.py
@@ -1,13 +1,14 @@
-#estudo sobre algoritmos de ordenação de arrays
+#estudo sobre algoritmos de ordenação
 lista = [10,8,12, 12, 1, 2, 44, 3, 2, 90, 12, 13, 2]
 
 def bubble_sort(lista:list) -> list:
     lista_ordenada = lista.copy()
-    for i in range(0, len(lista_ordenada,)):
-        for t in range(i +1, len(lista_ordenada)):
-            if lista_ordenada[i] > lista_ordenada[t]:
+    for i in range(0, len(lista_ordenada,)): # itera sobre os elementos da lista
+        for t in range(i +1, len(lista_ordenada)): # a cada elemento, itera sobre seus sucessores
+            if lista_ordenada[i] > lista_ordenada[t]: # se o elemento a seguir for maior que o atual os dois trocam de lugar
                 lista_ordenada[i], lista_ordenada[t] = lista_ordenada[t], lista_ordenada[i]
     return lista_ordenada
+    #algoritmo com notação O(n^2) 
 
 print(bubble_sort(lista))
 print(lista)
